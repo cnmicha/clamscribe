087b02a43316640a20bed5cdf12b047f580de6c9
cnmicha <evsc@gmx.de>
2014-12-10 20:34:00 +0100

    issue #clamscribe-12 Web: template integration

--- www/index.php
+++ www/index.php
@@ -6,9 +6,13 @@
  * Time: 17:28
  */
 
-//include classes?
+//include classes
 require_once('class/display.class.php');
 require_once('class/error.class.php');
+
+//include config
+require_once('config/config.inc.php');
+
 
 require 'libs/Smarty.class.php';
 
@@ -33,16 +37,45 @@ $oSmarty->cache_lifetime = 120;
 
 //module system
 if (isset($_GET['module'])) {
-    if (file_exists('module/' . $_GET['module'] . '/action.php') && file_exists('module/' . $_GET['module'] . '/template.tpl')) {
-        include_once('module/' . $_GET['module'] . '/action.php');
-        if (!cDisplay::getInstance()->isLocked()) $oSmarty->display('module/' . $_GET['module'] . '/template.tpl'); //if page content not locked (no err message) display template
+    if (file_exists('module/' . $_GET['module'] . '/action.php') && file_exists('module/' . $_GET['module'] . '/template.tpl') && file_exists('module/' . $_GET['module'] . '/config.php')) {
+        require_once('module/' . $_GET['module'] . '/config.php');
+        $oSmarty->assign('page_title', $aConfig['title']);
+        $oSmarty->assign('page_caption', $aConfig['caption']);
+        $oSmarty->assign('user_ip', $_SERVER['REMOTE_ADDR']);
+
+        require_once('module/' . $_GET['module'] . '/action.php');
+
+        if (!cDisplay::getInstance()->isBLocked()) {
+            $oSmarty->assign('tpl_file', '../module/' . $_GET['module'] . '/template.tpl');
+
+            $sUrl = (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . ROOT_URL_FROM_DOCROOT . '/index.php';
+            $oSmarty->assign('smarty_url', $sUrl);
+
+            $oSmarty->display('include/page.tpl');
+        } //if page content not locked (no err message) display template
+
     } else {
         cError::getInstance()->throwError(cError::ERR_TYPE_TEMPLATE);
     }
 } else {
-    if (file_exists('module/index/action.php') && file_exists('module/index/template.tpl')) {
-        include_once('module/index/action.php');
-        if (!cDisplay::getInstance()->isLocked()) $oSmarty->display('module/index/template.tpl'); //if page content not locked (no err message) display template
+    if (file_exists('module/index/action.php') && file_exists('module/index/template.tpl') && file_exists('module/index/config.php')) {
+        require_once('module/index/config.php');
+        $oSmarty->assign('page_title', $aConfig['title']);
+        $oSmarty->assign('page_caption', $aConfig['caption']);
+        $oSmarty->assign('user_ip', $_SERVER['REMOTE_ADDR']);
+
+        require_once('module/index/action.php');
+
+        if (!cDisplay::getInstance()->isBLocked()) {
+            $oSmarty->assign('tpl_file', '../module/index/template.tpl');
+
+            $sUrl = (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . ROOT_URL_FROM_DOCROOT . '/index.php';
+            $oSmarty->assign('smarty_url', $sUrl);
+
+            $oSmarty->display('include/page.tpl');
+        } //if page content not locked (no err message) display template
+
+        if (!cDisplay::getInstance()->isBLocked()) $oSmarty->display('module/index/template.tpl'); //if page content not locked (no err message) display template
     } else {
         cError::getInstance()->throwError(cError::ERR_TYPE_TEMPLATE);
     }
--- www/module/index/action.php
+++ www/module/index/action.php
@@ -5,8 +5,3 @@
  * Date: 08.12.2014
  * Time: 17:29
  */
-echo('awe');
-?>
-
-<h2>awesome.</h2>
-This project is going to be really cool!
