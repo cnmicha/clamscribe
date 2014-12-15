3b7baceb2fb9d8936caa7080907993f57f5e89e5
cnmicha <evsc@gmx.de>
2014-12-16 00:01:34 +0100

    issue #32 Web: recovery module password lost

---
+++ www/module/recovery/action.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: micha
+ * Date: 15.12.2014
+ * Time: 22:28
+ */
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //TODO
+}
---
+++ www/module/recovery/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: micha
+ * Date: 15.12.2014
+ * Time: 22:28
+ */
+
+$aConfig = array(
+    'title' => 'Password recovery',
+    'caption' => 'l',
+    'needsLogin' => false,
+    'template' => 'empty.tpl'
+);
