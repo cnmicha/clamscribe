d86cd83f3691011ec6d78e05a3217fa39eacaac2
cnmicha <evsc@gmx.de>
2014-12-21 14:39:05 +0100

    issue #34 Smarty: change password
    created module files
    integrated into menu in the upper-right corner in main template

---
+++ www/module/account/action.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: micha
+ * Date: 21.12.2014
+ * Time: 14:35
+ */
---
+++ www/module/account/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: micha
+ * Date: 21.12.2014
+ * Time: 14:36
+ */
+
+$aConfig = array(
+    'title' => 'Acoount',
+    'caption' => 'Change account settings',
+    'needsLogin' => true,
+    'template' => 'main.tpl'
+);
